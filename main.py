7c5bbd4bf4b16bb2b0b2aa1f1d4f8155d70c7036
Arvin Asgharian <Arvin.Asghrian@bio-techne.com>
2025-06-06 10:18:16 -0400

    changed to main.py

---
+++ main.py
@@ -0,0 +1,52 @@
+import os, yaml
+from argparse import ArgumentParser
+from util import fill_fields, encrypte_file, get_filename, create_mapping
+from pathlib import Path
+
+data = {
+    "ref_num" : "6350527431",
+    "start_date" : "27/05/2025",
+    "exp_date" : "31/05/2026",
+    "lot_num" : "242206PJ-A"
+}
+
+def main():
+    parser = ArgumentParser(" CoA creation program ", description=" This program uses pre-made templates alongside data from travelers to create CoA pdf")
+    parser.add_argument('--run-mode', type=str, default='prod')
+    parser.add_argument('--model', type=str, required=True) 
+    parser.add_argument('--verbose', action='store_true')
+
+    args = parser.parse_args()
+    config = None
+    dirc = args.model
+    rm = args.run_mode
+    with open(f"config.yaml", mode='r') as f:
+        config = yaml.safe_load(f)[rm]
+    
+    # Create the given CoAs using the provided data from MOPHO
+    info = config['models'][dirc]
+    path = fill_fields(Path(config['model_dir']) / Path(dirc), info, data)
+    info['FileName'] = get_filename()
+    
+
+    mapping = create_mapping(config, info, data)
+
+    # Output the data
+    for dir in config['pdf_output_dir']:
+        dir_p = Path(dir)
+        if (not os.path.exists(dir_p)):
+            os.makedirs(dir_p)
+        # mapping.to_csv(dir_p / Path(info['FileName']))
+        encrypte_file(path, dir_p / Path(info['FileName']).with_suffix('.csv'))
+    
+    for dir in config['pdf_output_dir']:
+        dir_p = Path(dir)
+        if (not os.path.exists(dir_p)):
+            os.makedirs(dir_p)
+        mapping.to_csv(dir_p / Path(info['FileName']).with_suffix('.csv'), index=False)
+    
+    os.remove(path)
+
+    
+
+main()
