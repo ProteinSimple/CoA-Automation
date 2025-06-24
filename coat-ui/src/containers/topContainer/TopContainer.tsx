f53bdc143e02d5490834692e26ae3196b6f8cae3
Arvin Asgharian <Arvin.Asghrian@bio-techne.com>
2025-06-24 15:58:18 -0400

    added loading to Gen button and some styiling to input and button

--- coat-ui/src/containers/topContainer/TopContainer.tsx
+++ coat-ui/src/containers/topContainer/TopContainer.tsx
@@ -1,6 +1,9 @@
 import "./TopContainer.css";
 import { pythonCoa } from "../../services";
 import { useCartridge } from "../../contexts";
+import { useState } from "react";
+import { DotLoader } from "react-spinners";
+
 
 interface TopContainerProps {
   setFilter: (given: string) => void;
@@ -8,10 +11,11 @@ interface TopContainerProps {
 
 function TopContainer({ setFilter }: TopContainerProps) {
   const { cartridgeList } = useCartridge();
+  const [ fetchFin, setFetchFin ] = useState(true)
 
   async function generate() {
+    setFetchFin(false)
     const outputed_files = await pythonCoa(cartridgeList);
-
     if (Array.isArray(outputed_files)) {
       const message = `Following files were created in the process of generation!\n\n${outputed_files.join(
         "\n"
@@ -21,6 +25,7 @@ function TopContainer({ setFilter }: TopContainerProps) {
       // fallback, but normally shouldn't hit here
       alert("Unexpected output format");
     }
+    setFetchFin(true)
   }
 
   return (
@@ -32,15 +37,18 @@ function TopContainer({ setFilter }: TopContainerProps) {
           generate();
         }}
       >
-        <div>
-          <input
-            id="SN-search"
-            placeholder="Search... 🔍"
-            onChange={(e) => setFilter(e.target.value)}
-          />
-          <input id="greet-input" placeholder="Enter a name..." />
-        </div>
-        <button type="submit">Generate</button>
+        
+        <input
+          id="SN-search"
+          placeholder="Search... 🔍"
+          onChange={(e) => setFilter(e.target.value)}
+        />
+        <input id="greet-input" placeholder="Enter a name..." />
+        
+        <button type="submit" disabled={!fetchFin}>
+          {fetchFin? "Generate" : 
+              <DotLoader color="white" loading={!fetchFin} size={20} />}
+       </button>
       </form>
     </div>
   );
