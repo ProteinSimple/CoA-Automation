597df748027025c00645da35e57720ea9ad6dfbc
Arvin Asgharian <Arvin.Asghrian@bio-techne.com>
2025-07-03 11:36:48 -0400

    working on it

--- coat-ui/src/components/cartridgeListItem/CartridgeListItem.tsx
+++ coat-ui/src/components/cartridgeListItem/CartridgeListItem.tsx
@@ -5,14 +5,13 @@ import { useCartridge } from "../../contexts";
 type ListItemProps = {
   id: string;
   time: string;
+  type: string;
 };
 
-function ListItem( { id, time }: ListItemProps) {
+function ListItem( { id, time, type }: ListItemProps) {
   const { selectedCartridgeList, addCartridge, removeCartridge } = useCartridge()
-   
-  const isChecked = (id: string) => {
-    return selectedCartridgeList.has(id)
-  }
+  const isChecked = (id: string) => { return selectedCartridgeList.has(id) }
+  const [datePart, timePart] = time.split(" ");
 
   return (
     <div className="list_item">
@@ -26,9 +25,15 @@ function ListItem( { id, time }: ListItemProps) {
           {id}
         </p> 
         <p className="list_item_date">
-          {time}
-        </p> 
+          {datePart}
+        </p>
+        <p className="list_item_date">
+          {timePart}
+        </p>
       </div>
+      <p>
+        {type}
+      </p>
     </div>
     
   )
